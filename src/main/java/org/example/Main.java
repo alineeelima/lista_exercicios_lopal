bb3948e3686d044762a9658a399abca1c3109107
alineeelima <aline810delima@gmail.com>
2026-08-28 16:21:27 -0300

    Feat: Exercício 1.2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,8 @@
 package org.example;
 public class Main {
     static void main() {
-        int valorInt = 12;
-        double valorDouble = valorInt;
-        System.out.println("Valor do double: " + valorDouble);
+        double valorDouble = 25.7;
+        int valorInt = (int) valorDouble;
+        System.out.println("Valor do inteiro: " + valorInt);
     }
 }
