a5df0e6da2b56c0b638491ebed9536825a29c297
alineeelima <aline810delima@gmail.com>
2026-08-28 16:31:03 -0300

    Feat: Exercício 3.5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,14 +3,11 @@ import java.util.Scanner;
 public class Main {
     static void main() {
         Scanner entrada = new Scanner(System.in);
-        double base;
-        System.out.println("Informe o tamanho da base do seu retangulo (em cm): ");
-        base = entrada.nextDouble();
-        double altura;
-        System.out.println("Informe a altura do seu retangulo (em cm): ");
-        altura = entrada.nextDouble();
-        double perimetro = (base * 2) + (altura * 2);
-        System.out.println("A altura e a base do seu retangulo e: " + base + " e " + altura);
-        System.out.println("A perimetro da sua figura e: " + perimetro);
+        double preco;
+        System.out.println("Informe o valor do seu produto: ");
+        preco = entrada.nextDouble();
+        double desconto = preco - ((preco * 15) / 100);
+        System.out.println("O valor do seu produto sem desconto e: " + preco);
+        System.out.println("O valor do seu produto com desconto e: " + desconto);
     }
 }
