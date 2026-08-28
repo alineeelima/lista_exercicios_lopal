74fcdaab12834e9f5bc28060ec18213b9e6de9f0
alineeelima <aline810delima@gmail.com>
2026-08-28 16:30:28 -0300

    Feat: Exercício 3.4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 public class Main {
     static void main() {
         Scanner entrada = new Scanner(System.in);
-        double lado;
-        System.out.println("Informe o tamanho do lado do seu quadrado (em cm): ");
-        lado = entrada.nextDouble();
-        double area = lado * lado;
-        System.out.println("A o tamanho do lado do seu quadrado e: " + lado);
-        System.out.println("A area da sua figura e: " + area + "cm2");
+        double base;
+        System.out.println("Informe o tamanho da base do seu retangulo (em cm): ");
+        base = entrada.nextDouble();
+        double altura;
+        System.out.println("Informe a altura do seu retangulo (em cm): ");
+        altura = entrada.nextDouble();
+        double perimetro = (base * 2) + (altura * 2);
+        System.out.println("A altura e a base do seu retangulo e: " + base + " e " + altura);
+        System.out.println("A perimetro da sua figura e: " + perimetro);
     }
 }
