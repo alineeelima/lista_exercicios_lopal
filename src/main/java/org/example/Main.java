44510c78b75582553e5af8da210ebb3e2f91863f
53602007820 <aline810delima@gmail.com>
2026-09-01 15:28:40 -0300

    Feat: Exercício 5.5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,13 +5,12 @@ import java.util.Scanner;
 public class Main {
     static void main() {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Digite o capital inicial: ");
-        double capIn = entrada.nextDouble();
-        System.out.println("Digite a taxa de juros: ");
-        double taxaJuros = entrada.nextDouble();
-        System.out.println("Digite o tempo da dívida (em meses): ");
-        double tempo = entrada.nextDouble();
-        double juros = capIn * taxaJuros * tempo;
-        System.out.println("O valor do seu juros (em juros simples) é de: "+ juros);
+        System.out.println("Digite um tempo (em segundos): ");
+        int segundos = entrada.nextInt();
+        int todosMinutos = segundos/60;
+        int horas = todosMinutos/60;
+        int minutos = todosMinutos%60;
+        int resto = segundos%60;
+        System.out.println("O seu tempo é igual a: "+ horas + " hora(s), " +minutos+ " minuto(s) e "+resto+" segundo(s)");
     }
 }
