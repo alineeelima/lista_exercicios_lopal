5dfb27eec3aeed477d8b14b8dc80f6b00bda3d3a
53602007820 <aline810delima@gmail.com>
2026-09-01 14:36:11 -0300

    Feat: Exercício 5.2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,16 @@
 package org.example;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
     static void main() {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Digite a sua altura (em m): ");
-        double altura = entrada.nextDouble();
-        System.out.println("Digite o seu peso (em kg): ");
-        double peso = entrada.nextDouble();
-        double imc = peso / (altura*altura);
-        System.out.println("O seu índice de massa corporal é igual a: "+ imc);
+        System.out.println("Digite uma medida em metros: ");
+        double metros = entrada.nextDouble();
+        double cm = metros * 100;
+        double mm = metros * 1000;
+        System.out.println("Medida em metros: "+ metros);
+        System.out.println("Medida em cm: "+ cm);
+        System.out.println("Medida em mm: "+ mm);
     }
 }
