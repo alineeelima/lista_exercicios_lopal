940a48baa7822ffde11706be827270f8adfa8dd9
53602007820 <aline810delima@gmail.com>
2026-09-01 14:48:07 -0300

    Feat: Exercício 5.4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class Main {
     static void main() {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Digite o dividendo da operação: ");
-        int num1 = entrada.nextInt();
-        System.out.println("Digite o divisor da operação: ");
-        int num2 = entrada.nextInt();
-        int div = num1/num2;
-        int resto = num1%num2;
-        System.out.println("O resultado da sua operação é de: "+ div);
-        System.out.println("O resto da sua operação é de: "+ resto);
+        System.out.println("Digite o capital inicial: ");
+        double capIn = entrada.nextDouble();
+        System.out.println("Digite a taxa de juros: ");
+        double taxaJuros = entrada.nextDouble();
+        System.out.println("Digite o tempo da dívida (em meses): ");
+        double tempo = entrada.nextDouble();
+        double juros = capIn * taxaJuros * tempo;
+        System.out.println("O valor do seu juros (em juros simples) é de: "+ juros);
     }
 }
