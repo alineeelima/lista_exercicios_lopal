ce0bad3af30b77b870206dae98f7c2493ef24fc8
53602007820 <aline810delima@gmail.com>
2026-09-01 14:16:42 -0300

    Feat: Exercício 4.5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,14 @@ import java.util.Scanner;
 public class Main {
     static void main() {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Digite qual é o raio do seu círculo (em cm): ");
-        double raio = entrada.nextDouble();
-        double area = Math.PI * raio*raio;
-        System.out.println("A área do seu círculo é de: " + area + "cm");
+        System.out.println("Digite o valor de A: ");
+        int A = entrada.nextInt();
+        System.out.println("Digite o valor de B: ");
+        int B = entrada.nextInt();
+        int troca = A;
+        A = B;
+        B = troca;
+        System.out.println("O novo valor de A é: "+ A);
+        System.out.println("O novo valor de B é: "+ B);
     }
 }
