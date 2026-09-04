4794b4a637f186bd32d1c48565c6f280c2b9c30a
53602007820 <aline810delima@gmail.com>
2026-09-04 13:40:18 -0300

    Feat: Exercício 4 - slides

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,11 +5,22 @@ import java.util.Scanner;
 public class Main {
     static void main() {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Digite um tempo (em segundos): ");
-        int segundos = entrada.nextInt();
-        int horas = segundos/3600;
-        int minutos = (segundos%3600)/60;
-        int resto = (segundos%3600)%60;
-        System.out.println("O seu tempo é igual a: "+ horas + " hora(s), " +minutos+ " minuto(s) e "+resto+" segundo(s)");
+        System.out.println("Digite a nota do aluno: ");
+        Double nota = entrada.nextDouble();
+        if (nota >= 7.5){
+            System.out.println("Excelente");
+        }
+        if (nota >= 5.00 && nota < 7.5){
+            System.out.println("Bom");
+        }
+        if (nota >= 2.5 && nota < 5.0){
+            System.out.println("Regular");
+        }
+        if (nota < 2.5){
+            System.out.println("Insuficiente");
+        }
+        if (nota < 0 || nota > 10){
+            System.out.println("Nota inválida");
+        }
     }
 }
