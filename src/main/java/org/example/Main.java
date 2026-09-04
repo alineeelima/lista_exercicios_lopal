749f2f810aeefcd24897d37f0bc0cd70cdaa358d
53602007820 <aline810delima@gmail.com>
2026-09-04 13:47:12 -0300

    Feat: Exercício 6 - slides

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,22 +5,12 @@ import java.util.Scanner;
 public class Main {
     static void main() {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Digite a nota do aluno: ");
-        Double nota = entrada.nextDouble();
-        if (nota >= 7.5){
-            System.out.println("Excelente");
-        }
-        if (nota >= 5.00 && nota < 7.5){
-            System.out.println("Bom");
-        }
-        if (nota >= 2.5 && nota < 5.0){
-            System.out.println("Regular");
-        }
-        if (nota < 2.5){
-            System.out.println("Insuficiente");
-        }
-        if (nota < 0 || nota > 10){
-            System.out.println("Nota inválida");
+        System.out.println("Qual é o salário mensal de um funcionário: ");
+        Double salario = entrada.nextDouble();
+        if (salario >= 4000){
+            System.out.println("Você não tem direito ao vale refeição!");
+        }else{
+            System.out.println("Você tem direito ao vale refeiçãõ!");
         }
     }
 }
