8880ef3c2f8b45d44edee7f4eec34939a1c6f45f
53602007820 <aline810delima@gmail.com>
2026-09-04 14:36:29 -0300

    Feat: Exercício 10 - slides

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class Main {
     static void main() {
         Scanner entrada = new Scanner(System.in);
-        System.out.print("Qual é o cargo do funcionário: ");
-        String resposta = entrada.nextLine();
+        System.out.print("Qual é o saláario mensal do cliente: ");
+        double salario = entrada.nextDouble();
+        System.out.print("Qual é o saláario mensal do cliente: ");
+        double parcela = entrada.nextDouble();
 
-        if (resposta.equalsIgnoreCase("ADMINISTRATIVO")) {
-            System.out.println("Pode participar do cruso de idiomas.");
-        }else if(resposta.equalsIgnoreCase("LIDERANÇA")){
-            System.out.println("Pode participar do cruso de idiomas.");
+        if (parcela >= ((salario*30)/100)) {
+            System.out.println("Sua solicitação foi negada.");
         }else{
-            System.out.println("Não pode particpar do curso de idiomas.");
+            System.out.println("Seu empréstimo foi aceito.");
         }
     }
 }
