829e992f64f97376d5aa36de7e682088ba3c244c
53602007820 <aline810delima@gmail.com>
2026-09-04 14:32:23 -0300

    Feat: Exercício 9 - slides

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,13 +5,15 @@ import java.util.Scanner;
 public class Main {
     static void main() {
         Scanner entrada = new Scanner(System.in);
-        System.out.print("O funcionário utiliza veículo próprio para trabalhar? (S/N): ");
+        System.out.print("Qual é o cargo do funcionário: ");
         String resposta = entrada.nextLine();
 
-        if (resposta.equalsIgnoreCase("SIM")) {
-            System.out.println("Ele pode solicitar auxílio combustível.");
-        } else {
-            System.out.println("Ele não pode solicitar auxílio combustível.");
+        if (resposta.equalsIgnoreCase("ADMINISTRATIVO")) {
+            System.out.println("Pode participar do cruso de idiomas.");
+        }else if(resposta.equalsIgnoreCase("LIDERANÇA")){
+            System.out.println("Pode participar do cruso de idiomas.");
+        }else{
+            System.out.println("Não pode particpar do curso de idiomas.");
         }
     }
 }
