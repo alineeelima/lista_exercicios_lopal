9d1e18e1da4b75ffc110c80e0289fb8251e50daf
53602007820 <aline810delima@gmail.com>
2026-09-11 14:14:13 -0300

    Feat: Exercício 03 - slides 11/09

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,12 @@ import java.util.Scanner;
 
 public class Main {
     static void main() {
-        int peca = 1;
-        while (peca <= 20){
-            System.out.println("peça produzida");
-            peca++;
+        Scanner entrada = new Scanner(System.in);
+        double total = 0;
+        for(int venda=1;venda<=5;venda++){
+            System.out.println("Digite o valor da compra "+ venda + ": " );
+            total = total + entrada.nextDouble();
         }
+        System.out.println("O faturamento total é de: R$"+ total);
     }
 }
