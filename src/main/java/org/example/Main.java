a8c8e3d8e2bed12b135beba5b4b602622d7bae37
53602007820 <aline810delima@gmail.com>
2026-09-11 13:42:36 -0300

    Feat: Exercício 13 - slides

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,16 +5,13 @@ import java.util.Scanner;
 public class Main {
     static void main() {
         Scanner entrada = new Scanner(System.in);
-        System.out.print("Digite a renda do cliente: ");
-        double renda = entrada.nextDouble();
+        System.out.print("Digite o valor da transação: ");
+        double transacao = entrada.nextDouble();
 
-        System.out.print("Digite o score do cliente: ");
-        double score = entrada.nextDouble();
-
-        if (renda > 8000 && score > 700) {
-            System.out.println("Você pode solicitar o cartão premium.");
+        if (transacao > 10000) {
+            System.out.println("Cuidado! Transação suspeita.");
         }else{
-            System.out.println("Você não pode solicitar o cartão preimum.");
+            System.out.println("Essa transação é segura.");
         }
     }
 }
