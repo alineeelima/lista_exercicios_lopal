64e747816f3194abec0edfb08cfebdee306773bc
53602007820 <aline810delima@gmail.com>
2026-09-11 13:46:10 -0300

    Feat: Exercício 14 - slides

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class Main {
     static void main() {
         Scanner entrada = new Scanner(System.in);
-        System.out.print("Digite o valor da transação: ");
-        double transacao = entrada.nextDouble();
+        System.out.print("Digite o saldo médio mensal do cliente: ");
+        double saldoMedio = entrada.nextDouble();
 
-        if (transacao > 10000) {
-            System.out.println("Cuidado! Transação suspeita.");
+        if (saldoMedio > 5000) {
+            System.out.println("Você tem direito a isenção de tarifa.");
         }else{
-            System.out.println("Essa transação é segura.");
+            System.out.println("Você não tem direito a isenção de tarifa.");
         }
     }
 }
