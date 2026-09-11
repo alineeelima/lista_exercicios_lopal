589b9d4a58005ea78b6d15c4ba6359819a0dc516
53602007820 <aline810delima@gmail.com>
2026-09-11 13:56:24 -0300

    Feat: Exercício 02 - slides 11/09

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class Main {
     static void main() {
-        int contador = 1;
-        while (contador <= 10){
-            System.out.println("Partcipante cadastrado");
-            contador++;
+        int peca = 1;
+        while (peca <= 20){
+            System.out.println("peça produzida");
+            peca++;
         }
     }
 }
